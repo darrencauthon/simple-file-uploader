1076272a579947d0116e45ef56e3779835210893
Mert Yazicioglu <mert@MacBook-Air.local>
2015-08-16 16:55:38 +0300

    Version 0.2.0
    - Clicking the area now opens a file dialog to enable non-drag-and-drop uploads.
    - Rewrote the parts that were using jQuery and removed jQuery from the project.
    - Simplified the look and feel and made it small-screen-friendly.

--- router.js
+++ router.js
@@ -1,19 +1,21 @@
 function respondWithHTTPCode(response, code) {
-	response.writeHead(code, {'Content-Type': 'text/plain'});
-	response.end();
+  response.writeHead(code, {'Content-Type': 'text/plain'});
+  response.end();
 }
 
 function route(handle, pathname, response, postData) {
 
-	var extension = pathname.split('.').pop();
+  var extension = pathname.split('.').pop();
 
-	if ('function' === typeof handle[pathname]) {
-		handle[pathname](response, postData);
-	} else if ('css' === extension || 'js' === extension) {
-		handle._static(response, pathname, postData);
-	} else {
-		respondWithHTTPCode(response, 404);
-	}
+  if ( 'function' === typeof handle[pathname] ) {
+    handle[pathname](response, postData);
+  } else if ( 'css' === extension || 'js' === extension ) {
+    handle._static(response, pathname, postData);
+  } else {
+    respondWithHTTPCode(response, 404);
+  }
 }
 
-exports.route = route;
+module.exports = {
+  route: route
+};
